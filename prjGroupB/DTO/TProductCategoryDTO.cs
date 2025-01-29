3ea45f6750ca655cffb9f79cb15cdbb76ab120e9
Riley <heyimcy@gmail.com>
2025-01-30 01:25:54 +0800

    商品Display API ok

---
+++ prjGroupB/DTO/TProductCategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace prjGroupB.DTO
+{
+    public class TProductCategoryDTO
+    {
+        public int FProductCategoryId { get; set; }
+        public string FCategoryName { get; set; }
+        public int ProductCount { get; set; } // 類別中的商品數量
+    }
+}
---
+++ prjGroupB/DTO/TProductDetailDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace prjGroupB.DTO
+{
+    public class TProductDetailDTO
+    {
+        public int FProductId { get; set; }
+        public int? FProductCategoryId { get; set; }
+        public string FProductName { get; set; }
+        public decimal? FProductPrice { get; set; }
+        public string? FProductDescription { get; set; }
+        public bool? FIsOnSales { get; set; }
+        public int? FStock { get; set; }
+        public DateTime? FProductDateAdd { get; set; }
+        public DateTime? FProductUpdated { get; set; }
+        public string[] FImage { get; set; } = Array.Empty<string>();  // 若有圖片但無資料，可以返回空陣列；若無圖片，則返回 null。
+
+        //public int? FUserId { get; set; }
+        //public string? FUserNickName { get; set; }
+        //public string? FUserImage { get; set; } // Base64 縮圖
+
+    }
+}
