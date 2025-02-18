9040c4e0465c7d9b7b15da1281e030905ef44c1d
Daniel Chen <ahm12345@gmail.com>
2025-02-19 01:07:31 +0800

    新增 OpenWeather API 修改 Attraction Comment API 的功能

---
+++ prjGroupB/Controllers/OpenWeatherAPIController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace prjGroupB.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenWeatherAPIController : ControllerBase {
+        private readonly IConfiguration _config;
+        private readonly HttpClient _client;
+
+        public OpenWeatherAPIController(IConfiguration config, HttpClient client) {
+            _config = config;
+            _client = client;
+        }
+
+        // 取得天氣圖示
+        // GET: api/WeatherIcon
+        // https://localhost:7112/api/OpenWeatherAPI/WeatherIcon?lat=25.070843504702268&lon=121.49929878012719
+        [HttpGet("WeatherIcon")]
+        public async Task<ActionResult<string>> GetWeatherIcon(float lat, float lon) {
+            string apiKey = _config["OpenWeather:ApiKey"];
+            
+            if(apiKey==null || apiKey == "") {
+                return BadRequest("API Key 未設定");
+            }
+            // https://api.openweathermap.org/data/2.5/weather?lat=25.070843504702268&lon=121.49929878012719&appid=11005260cd604454569d8cd051212258
+            string requestUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}";
+            HttpResponseMessage response = await _client.GetAsync(requestUrl);
+
+            // 解析 JSON
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            using var jsonDoc = JsonDocument.Parse(jsonResponse);
+            var root = jsonDoc.RootElement;
+
+            if (!root.TryGetProperty("weather", out var weatherArray) || weatherArray.GetArrayLength() == 0) {
+                return BadRequest("找不到天氣資訊");
+            }
+
+            string iconCode = weatherArray[0].GetProperty("icon").GetString();
+            string iconUrl = $"https://openweathermap.org/img/wn/{iconCode}@2x.png";
+
+            return Ok(iconUrl);
+        }
+    }
+}
--- prjGroupB/DTO/TAttractionCommentDTO.cs
+++ prjGroupB/DTO/TAttractionCommentDTO.cs
@@ -3,21 +3,14 @@
 namespace prjGroupB.DTO {
     public class TAttractionCommentDTO {
         public int FCommentId { get; set; }
-
         public int? FAttractionId { get; set; }
-
         public string? FAttractionName { get; set; }
-
         public int? FUserId { get; set; }
-
         public string? FUserName { get; set; }
-
         public string? FUserNickName { get; set; }
-
+        public string? FUserImage { get; set; }
         public int? FRating { get; set; }
-
         public string? FComment { get; set; }
-
         public DateTime? FCreatedDate { get; set; }
     }
 }
