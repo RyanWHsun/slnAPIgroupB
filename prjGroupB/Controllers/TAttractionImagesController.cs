7cb340d65bd6debd943f310db5c3abdbb4e21729
ChenLiCh <ahm12345@gmail.com>
2025-03-01 03:18:22 +0800

    加了一些防錯機制

--- prjGroupB/Controllers/TAttractionImagesController.cs
+++ prjGroupB/Controllers/TAttractionImagesController.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,161 +9,222 @@ using Microsoft.EntityFrameworkCore;
 using prjGroupB.DTO;
 using prjGroupB.Models;
 
-namespace prjGroupB.Controllers {
+namespace prjGroupB.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class TAttractionImagesController : ControllerBase {
+    public class TAttractionImagesController : ControllerBase
+    {
         private readonly dbGroupBContext _context;
 
-        public TAttractionImagesController(dbGroupBContext context) {
+        public TAttractionImagesController(dbGroupBContext context)
+        {
             _context = context;
         }
 
         // GET: api/TAttractionImages
         [HttpGet]
-        public async Task<IEnumerable<TAttractionImageDTO>> GetTAttractionImages() {
-            var attractionImageDTOs = await _context.TAttractionImages.Select(
-                attractionImage => new TAttractionImageDTO {
-                    FAttractionId = attractionImage.FAttractionId,
-                    FAttractionName = attractionImage.FAttraction.FAttractionName,
-                    FAttractionImageId = attractionImage.FAttractionImageId,
-                    FImage = attractionImage.FImage
-                    //FImage = Convert.ToBase64String(attractionImage.FImage)
-                }).ToListAsync();
-            return attractionImageDTOs;
+        public async Task<ActionResult<IEnumerable<TAttractionImageDTO>>> GetTAttractionImages()
+        {
+            try
+            {
+                var attractionImageDTOs = await _context.TAttractionImages.Select(
+                    attractionImage => new TAttractionImageDTO
+                    {
+                        FAttractionId = attractionImage.FAttractionId,
+                        FAttractionName = attractionImage.FAttraction.FAttractionName,
+                        FAttractionImageId = attractionImage.FAttractionImageId,
+                        FImage = attractionImage.FImage
+                    }).ToListAsync();
+                return Ok(attractionImageDTOs);
+            }
+            catch (DbException ex)
+            {
+                // 資料庫連線錯誤
+                return StatusCode(500, new { error = "資料庫連線錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
         // GET: api/TAttractionImages/5
         // id is the attraction id
         [HttpGet("{id}")]
-        public async Task<IEnumerable<TAttractionImageDTO>> GetTAttractionImage(int id) {
-            var attractionImages = await _context.TAttractionImages
-                .Include(image => image.FAttraction)
-                .Where(image => image.FAttractionId == id).ToListAsync();
+        public async Task<ActionResult<IEnumerable<TAttractionImageDTO>>> GetTAttractionImage(int id)
+        {
+            try
+            {
+                var attractionImages = await _context.TAttractionImages
+                    .Include(image => image.FAttraction)
+                    .Where(image => image.FAttractionId == id).ToListAsync();
 
-            // .Any() 是 LINQ 的一個方法，檢查集合中是否存在至少一個元素。
-            // 如果集合中有資料，.Any() 會回傳 true。
-            // 如果集合為空，.Any() 會回傳 false。
-            if (attractionImages == null || !attractionImages.Any()) {
-                return new List<TAttractionImageDTO>();
-            }
-
-            var attractionImageDTOs = attractionImages.Select(
-                attractionImage => new TAttractionImageDTO {
-                    FAttractionId = attractionImage.FAttractionId,
-                    FAttractionName = attractionImage.FAttraction.FAttractionName,
-                    FAttractionImageId = attractionImage.FAttractionImageId,
-                    FImage = attractionImage.FImage
-                    //FImage = Convert.ToBase64String(attractionImage.FImage) // 轉換 byte[] 為 Base64 字串
+                // .Any() 是 LINQ 的一個方法，檢查集合中是否存在至少一個元素。
+                // 如果集合中有資料，.Any() 會回傳 true。
+                // 如果集合為空，.Any() 會回傳 false。
+                if (attractionImages == null || !attractionImages.Any())
+                {
+                    return NotFound(new { error = "找不到相關圖片" });
                 }
-            ).ToList();
 
-            return attractionImageDTOs;
+                var attractionImageDTOs = attractionImages.Select(
+                    attractionImage => new TAttractionImageDTO
+                    {
+                        FAttractionId = attractionImage.FAttractionId,
+                        FAttractionName = attractionImage.FAttraction.FAttractionName,
+                        FAttractionImageId = attractionImage.FAttractionImageId,
+                        FImage = attractionImage.FImage
+                    }).ToList();
+
+                return Ok(attractionImageDTOs);
+            }
+            catch (DbException ex)
+            {
+                // 資料庫連線錯誤
+                return StatusCode(500, new { error = "資料庫連線錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
         // GET: api/TAttractionImages/Search?id=5&amount=1
         // 根據 attraction ID 取得圖片(可選張數)
         [HttpGet("Search")]
-        public async Task<TAttractionImageDTO> GetOneTAttractionImage(int id, int amount = 1) {
-            var attractionImage = await _context.TAttractionImages.Include(image=>image.FAttraction).Where(image => image.FAttractionId == id).FirstOrDefaultAsync();
-            // .Any() 是 LINQ 的一個方法，檢查集合中是否存在至少一個元素。
-            // 如果集合中有資料，.Any() 會回傳 true。
-            // 如果集合為空，.Any() 會回傳 false。
-            if (attractionImage == null) {
-                return null;
+        public async Task<ActionResult<TAttractionImageDTO>> GetOneTAttractionImage(int id, int amount = 1)
+        {
+            try
+            {
+                var attractionImage = await _context.TAttractionImages
+                    .Include(image => image.FAttraction)
+                    .Where(image => image.FAttractionId == id)
+                    .FirstOrDefaultAsync();
+
+                // .Any() 是 LINQ 的一個方法，檢查集合中是否存在至少一個元素。
+                // 如果集合中有資料，.Any() 會回傳 true。
+                // 如果集合為空，.Any() 會回傳 false。
+                if (attractionImage == null)
+                {
+                    return NotFound(new { error = "找不到相關圖片" });
+                }
+
+                var attractionImageDTO = new TAttractionImageDTO
+                {
+                    FAttractionId = attractionImage.FAttractionId,
+                    FAttractionName = attractionImage.FAttraction.FAttractionName,
+                    FAttractionImageId = attractionImage.FAttractionImageId,
+                    FImage = attractionImage.FImage
+                };
+
+                return Ok(attractionImageDTO);
             }
-
-            var attractionImageDTO = new TAttractionImageDTO {
-                FAttractionId = attractionImage.FAttractionId,
-                FAttractionName = attractionImage.FAttraction.FAttractionName,
-                FAttractionImageId = attractionImage.FAttractionImageId,
-                FImage = attractionImage.FImage
-            };
-
-            return attractionImageDTO;
+            catch (DbException ex)
+            {
+                // 資料庫連線錯誤
+                return StatusCode(500, new { error = "資料庫連線錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
-
-        // PUT: api/TAttractionImages/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutTAttractionImage(int id, TAttractionImage tAttractionImage) {
-        //    if (id != tAttractionImage.FAttractionImageId) {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(tAttractionImage).State = EntityState.Modified;
-
-        //    try {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException) {
-        //        if (!TAttractionImageExists(id)) {
-        //            return NotFound();
-        //        }
-        //        else {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
 
         // POST: api/TAttractionImages
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<IActionResult> PostTAttractionImage([FromForm] int fAttractionId, [FromForm] List<IFormFile> fImages) {
-            if (fImages == null || fImages.Count == 0) {
-                return BadRequest("未選擇任何圖片");
+        public async Task<ActionResult<IEnumerable<TAttractionImageDTO>>> PostTAttractionImage([FromForm] int fAttractionId, [FromForm] List<IFormFile> fImages)
+        {
+            try
+            {
+                if (fImages == null || fImages.Count == 0)
+                {
+                    return BadRequest(new { error = "未選擇任何圖片" });
+                }
+
+                var uploadedImages = new List<TAttractionImageDTO>();
+
+                foreach (var image in fImages)
+                {
+                    using var memoryStream = new MemoryStream();
+                    await image.CopyToAsync(memoryStream);
+                    var imageData = memoryStream.ToArray();
+
+                    TAttractionImage attractionImage = new TAttractionImage
+                    {
+                        FAttractionId = fAttractionId,
+                        FImage = imageData
+                    };
+
+                    // 1. 新的記錄插入資料庫。
+                    // 2. 資料庫生成並返回新的 FAttractionImageId。
+                    // 3. EF 將新生成的 ID 更新到 attractionImage.FAttractionImageId。
+                    _context.TAttractionImages.Add(attractionImage);
+                    await _context.SaveChangesAsync();
+
+                    uploadedImages.Add(new TAttractionImageDTO
+                    {
+                        FAttractionId = attractionImage.FAttractionId,
+                        FAttractionName = "",
+                        FAttractionImageId = attractionImage.FAttractionImageId,
+                        FImage = attractionImage.FImage
+                    });
+                }
+
+                return Ok(uploadedImages);
             }
-
-            List<TAttractionImageDTO> uploadedImages = new List<TAttractionImageDTO>();
-
-            foreach (var image in fImages) {
-                using var memoryStream = new MemoryStream();
-                await image.CopyToAsync(memoryStream);
-                var imageData = memoryStream.ToArray();
-
-                TAttractionImage attractionImage = new TAttractionImage {
-                    FAttractionId = fAttractionId,
-                    FImage = imageData
-                };
-
-                // 1. 新的記錄插入資料庫。
-                // 2. 資料庫生成並返回新的 FAttractionImageId。
-                // 3. EF 將新生成的 ID 更新到 attractionImage.FAttractionImageId。
-                _context.TAttractionImages.Add(attractionImage);
-                await _context.SaveChangesAsync();
-
-                uploadedImages.Add(new TAttractionImageDTO {
-                    FAttractionId = attractionImage.FAttractionId,
-                    FAttractionName = "",
-                    FAttractionImageId = attractionImage.FAttractionImageId,
-                    FImage = attractionImage.FImage
-                });
+            catch (DbUpdateException ex)
+            {
+                // 資料庫更新錯誤
+                return StatusCode(500, new { error = "資料庫更新錯誤", details = ex.Message });
             }
-            return Ok(uploadedImages);
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
         // DELETE: api/TAttractionImages/5
         // id is the attraction id
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTAttractionImage(int id) {
-            var attractionImages = await _context.TAttractionImages
-                .Include(image => image.FAttraction)
-                .Where(image => image.FAttractionId == id).ToListAsync();
+        public async Task<IActionResult> DeleteTAttractionImage(int id)
+        {
+            try
+            {
+                var attractionImages = await _context.TAttractionImages
+                    .Include(image => image.FAttraction)
+                    .Where(image => image.FAttractionId == id).ToListAsync();
 
-            if (attractionImages == null || !attractionImages.Any()) {
-                return NotFound();
+                if (attractionImages == null || !attractionImages.Any())
+                {
+                    return NotFound(new { error = "找不到相關圖片" });
+                }
+
+                // 刪除所有查詢到的圖片
+                _context.TAttractionImages.RemoveRange(attractionImages);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
-
-            // 刪除所有查詢到的圖片
-            _context.TAttractionImages.RemoveRange(attractionImages);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (DbUpdateException ex)
+            {
+                // 資料庫更新錯誤
+                return StatusCode(500, new { error = "資料庫更新錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
-        private bool TAttractionImageExists(int id) {
+        private bool TAttractionImageExists(int id)
+        {
             return _context.TAttractionImages.Any(e => e.FAttractionImageId == id);
         }
     }
--- prjGroupB/Controllers/TAttractionUserFavoritesController.cs
+++ prjGroupB/Controllers/TAttractionUserFavoritesController.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,111 +9,136 @@ using Microsoft.EntityFrameworkCore;
 using prjGroupB.DTO;
 using prjGroupB.Models;
 
-namespace prjGroupB.Controllers {
+namespace prjGroupB.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class TAttractionUserFavoritesController : ControllerBase {
+    public class TAttractionUserFavoritesController : ControllerBase
+    {
         private readonly dbGroupBContext _context;
 
-        public TAttractionUserFavoritesController(dbGroupBContext context) {
+        public TAttractionUserFavoritesController(dbGroupBContext context)
+        {
             _context = context;
         }
-
-        // GET: api/TAttractionUserFavorites
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<TAttractionUserFavorite>>> GetTAttractionUserFavorites()
-        //{
-        //    return await _context.TAttractionUserFavorites.ToListAsync();
-        //}
 
         // GET: api/TAttractionUserFavorites/5
         // id is user id
         [HttpGet("{id}")]
-        public async Task<IEnumerable<TAttractionUserFavoriteDTO>> GetTAttractionUserFavorite(int id) {
-            var favorites = await _context.TAttractionUserFavorites
-                .Include(favorite => favorite.FAttraction)
-                .Include(favorite => favorite.FUser)
-                .Where(favorite => favorite.FUserId == id)
-                .ToListAsync();
+        public async Task<ActionResult<IEnumerable<TAttractionUserFavoriteDTO>>> GetTAttractionUserFavorite(int id)
+        {
+            try
+            {
+                var favorites = await _context.TAttractionUserFavorites
+                    .Include(favorite => favorite.FAttraction)
+                    .Include(favorite => favorite.FUser)
+                    .Where(favorite => favorite.FUserId == id)
+                    .ToListAsync();
 
-            if (favorites == null) {
-                return null;
+                if (favorites == null || !favorites.Any())
+                {
+                    return NotFound(new { error = "找不到收藏的景點" });
+                }
+
+                var favoriteDTOs = favorites.Select(favorite => new TAttractionUserFavoriteDTO
+                {
+                    FFavoriteId = favorite.FFavoriteId,
+                    FUserId = favorite.FUserId,
+                    FUsername = favorite.FUser.FUserName,
+                    FAttractionId = favorite.FAttractionId,
+                    FAttractionName = favorite.FAttraction.FAttractionName
+                }).ToList();
+
+                return Ok(favoriteDTOs);
             }
-
-            var favoriteDTOs = favorites.Select(favorite => new TAttractionUserFavoriteDTO {
-                FFavoriteId = favorite.FFavoriteId,
-                FUserId = favorite.FUserId,
-                FUsername = favorite.FUser.FUserName,
-                FAttractionId = favorite.FAttractionId,
-                FAttractionName = favorite.FAttraction.FAttractionName
-            });
-
-            return favoriteDTOs;
+            catch (DbException ex)
+            {
+                // 資料庫連線錯誤
+                return StatusCode(500, new { error = "資料庫連線錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
-
-        // PUT: api/TAttractionUserFavorites/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutTAttractionUserFavorite(int id, TAttractionUserFavorite tAttractionUserFavorite)
-        //{
-        //    if (id != tAttractionUserFavorite.FFavoriteId)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(tAttractionUserFavorite).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!TAttractionUserFavoriteExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
 
         // POST: api/TAttractionUserFavorites
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<TAttractionUserFavoriteDTO>> PostTAttractionUserFavorite(TAttractionUserFavorite tAttractionUserFavorite) {
-            _context.TAttractionUserFavorites.Add(tAttractionUserFavorite);
-            await _context.SaveChangesAsync();
+        public async Task<ActionResult<TAttractionUserFavoriteDTO>> PostTAttractionUserFavorite(TAttractionUserFavorite tAttractionUserFavorite)
+        {
+            try
+            {
+                if (tAttractionUserFavorite == null)
+                {
+                    return BadRequest(new { error = "收藏資訊不能為空" });
+                }
 
-            var favoriteDTO = new TAttractionUserFavoriteDTO {
-                FFavoriteId = tAttractionUserFavorite.FFavoriteId,
-                FUserId = tAttractionUserFavorite.FUserId,
-                FUsername = tAttractionUserFavorite.FUser.FUserName,
-                FAttractionId = tAttractionUserFavorite.FAttractionId,
-                FAttractionName = tAttractionUserFavorite.FAttraction.FAttractionName
-            };
+                _context.TAttractionUserFavorites.Add(tAttractionUserFavorite);
+                await _context.SaveChangesAsync();
 
-            return favoriteDTO;
+                var favoriteDTO = new TAttractionUserFavoriteDTO
+                {
+                    FFavoriteId = tAttractionUserFavorite.FFavoriteId,
+                    FUserId = tAttractionUserFavorite.FUserId,
+                    FUsername = tAttractionUserFavorite.FUser.FUserName,
+                    FAttractionId = tAttractionUserFavorite.FAttractionId,
+                    FAttractionName = tAttractionUserFavorite.FAttraction.FAttractionName
+                };
+
+                return CreatedAtAction(nameof(GetTAttractionUserFavorite), new { id = favoriteDTO.FUserId }, favoriteDTO);
+            }
+            catch (DbUpdateException ex)
+            {
+                // 資料庫更新錯誤
+                return StatusCode(500, new { error = "資料庫更新錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
         // DELETE: api/TAttractionUserFavorites
         [HttpDelete]
-        public async Task<IActionResult> DeleteTAttractionUserFavorite(TAttractionUserFavorite tAttractionUserFavorite) {            
-            if (tAttractionUserFavorite == null) {
-                return NotFound();
+        public async Task<IActionResult> DeleteTAttractionUserFavorite(TAttractionUserFavorite tAttractionUserFavorite)
+        {
+            try
+            {
+                if (tAttractionUserFavorite == null)
+                {
+                    return BadRequest(new { error = "刪除的收藏資訊不能為空" });
+                }
+
+                var existingFavorite = await _context.TAttractionUserFavorites
+                    .FirstOrDefaultAsync(f => f.FFavoriteId == tAttractionUserFavorite.FFavoriteId);
+
+                if (existingFavorite == null)
+                {
+                    return NotFound(new { error = "找不到要刪除的收藏" });
+                }
+
+                _context.TAttractionUserFavorites.Remove(existingFavorite);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
-
-            _context.TAttractionUserFavorites.Remove(tAttractionUserFavorite);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (DbUpdateException ex)
+            {
+                // 資料庫更新錯誤
+                return StatusCode(500, new { error = "資料庫更新錯誤", details = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                // 一般錯誤
+                return StatusCode(500, new { error = "內部錯誤", details = ex.Message });
+            }
         }
 
-        private bool TAttractionUserFavoriteExists(int id) {
+        private bool TAttractionUserFavoriteExists(int id)
+        {
             return _context.TAttractionUserFavorites.Any(e => e.FFavoriteId == id);
         }
     }
