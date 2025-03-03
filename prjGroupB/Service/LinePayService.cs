035a58a8102df473418ae561388dde0536a78caf
Riley <heyimcy@gmail.com>
2025-03-03 12:52:54 +0800

    LinePay網址改回localhost

--- prjGroupB/Service/LinePayService.cs
+++ prjGroupB/Service/LinePayService.cs
@@ -17,7 +17,7 @@ public class LinePayService
     private const string CHANNEL_ID = "2006949561";
     private const string CHANNEL_SECRET = "1724fe3b7e82ea6bd7cf8cfcd91f0d4a";
 
-    private const string FRONTEND_BASE_URL = "https://special-publicly-humpback.ngrok-free.app/"; // 你的前端網址
+    private const string FRONTEND_BASE_URL = "http://localhost:4200"; // 你的前端網址
 
     public LinePayService(HttpClient httpClient)
     {
@@ -50,7 +50,7 @@ public class LinePayService
             redirectUrls = new
             {
                 confirmUrl = $"{FRONTEND_BASE_URL}",
-                cancelUrl = "https://special-publicly-humpback.ngrok-free.app/"
+                cancelUrl = "http://localhost:4200"
             }
         };
 
